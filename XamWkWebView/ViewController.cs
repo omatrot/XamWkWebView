8e3d42da0da67a0aa3bbac58a0da9079f478f992
Olivier Matrot <olivier.matrot@rte.fr>
2021-05-20 09:35:10 +0200

    Second commit

---
+++ XamWkWebView/ViewController.cs
@@ -0,0 +1,52 @@
+﻿using Foundation;
+using System;
+using System.IO;
+using System.Text;
+using UIKit;
+using WebKit;
+
+namespace XamWkWebView
+{
+    public partial class ViewController : UIViewController
+    {
+        WKWebView wKWebView;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            wKWebView = new WKWebView(View.Frame, new WKWebViewConfiguration());
+            View.AddSubview(wKWebView);
+
+            var bundle = NSBundle.MainBundle;
+            var path = bundle.PathForResource("map", "html");
+            var html = string.Empty;
+            try
+            {
+                html = File.ReadAllText(path, Encoding.UTF8);
+            }
+            catch
+            {
+                //ERROR
+            }
+            wKWebView.LoadHtmlString(html, null);
+            wKWebView.AllowsBackForwardNavigationGestures = true;
+        }
+
+        public override void LoadView()
+        {
+            base.LoadView();
+
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
